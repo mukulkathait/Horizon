f1876cd079eca7b38ec5e868e6d55f6e7ce40f46
Mukul Kathait <kathaitmukul0699@gmail.com>
2024-04-02 10:30:13 +0530

    util functions and models added

---
+++ src/server.js
@@ -0,0 +1,45 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(
+        `App is listening on port http://localhost:${process.env.PORT}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB connection FAILED : ", error);
+  });
+
+/* import express from "express";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `App is listening on port http://localhost:${process.env.PORT}`
+      );
+    });
+  } catch (error) {
+    console.error("ERROR:", error);
+    throw err;
+  }
+})(); */
